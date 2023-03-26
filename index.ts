5649a63f4673f1345d3c9bc637920cdc1b69ea03
Cito2310 <cito2310@gmail.com>
2023-03-26 03:58:33 -0300

    added code basic to encode and decode img-base64

---
+++ index.ts
@@ -0,0 +1,7 @@
+import fs from "fs";
+import { writeFileSync } from 'fs';
+
+let avatar = fs.readFileSync('./deerColorado.jpg');
+const avatar_64 = Buffer.from(avatar).toString('base64');
+
+writeFileSync("./test.png", avatar_64, { encoding: "base64" })
